ddef25f2191995c1f3736d66fbabd7138486f409
Jeferson Lemos <125606971+jefflemos-ezy@users.noreply.github.com>
2024-05-26 16:02:50 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "null_resource" "testing" {
+resource "local_file" "testing" {
+  content  = var.resource_content
+  filename = "foo.bar"
+}
 
-
+variable "resource_content" {
+  type = string
+  default = "this is the content of the file"
 }
